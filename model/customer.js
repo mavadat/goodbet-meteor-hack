ca3cb4128e0cfc66f6d1e3121edc0e0b601cc9df
Jamal Mavadat <jamal@mavadat.net>
2015-02-15 19:40:09 +1100

    Add support for customer to initialize aggregate data

--- model/customer.js
+++ model/customer.js
@@ -1,5 +1,13 @@
 Customer = function(id) {
+    //actual data
     this.id = id;
+
+    //computed data
+    this.settledBetCount = 0;
+    this.settledStakeSum = 0;
+    this.winCount = 0;
+    this.unsettledBetCount = 0;
+    this.unsettledStakeSum = 0;
 };
 
 //events
--- tests/jasmine/client/unit/single.bet.aggregation.spec.js
+++ tests/jasmine/client/unit/single.bet.aggregation.spec.js
@@ -8,11 +8,11 @@ describe('', function () {
 
     beforeEach(function () {
         worksheet = new Worksheet();
-        customer = worksheet.customer.getOrCreate(1);
-        event = worksheet.event.getOrCreate(1);
-        participant = worksheet.participant.getOrCreate(1);
+        customer = worksheet.customers.getOrCreate(1);
+        event = worksheet.events.getOrCreate(1);
+        participant = worksheet.participants.getOrCreate(1);
     });
-    
+
     describe('Single bet aggregate calculation', function () {
         it('should have no settled/unsettled bet history when no settled bet is imported for', function () {
             //arrange
